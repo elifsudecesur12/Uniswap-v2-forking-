e668cc185f99952d278b307d4f2c4611d1301816
elif sude cesur <121366431+elifsudecesur12@users.noreply.github.com>
2023-10-24 00:25:02 +0300

    Update amm.sol

--- amm.sol
+++ amm.sol
@@ -1,102 +1,111 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
-contract AMMExample is IUniswapV2Callee, Ownable {
-    IUniswapV2Factory public uniswapV2Factory;
-    IUniswapV2Pair public pair;
+contract AMMExample is Ownable {
     IERC20 public token;
-    address public priceOracleAddress;
+    address public liquidityPoolAddress;
+    uint256 public feePercentage;
     
-    constructor(address _uniswapV2Factory, address _token, address _priceOracleAddress) {
-        uniswapV2Factory = IUniswapV2Factory(_uniswapV2Factory);
+    constructor(address _token, address _liquidityPoolAddress, uint256 _feePercentage) {
         token = IERC20(_token);
-        priceOracleAddress = _priceOracleAddress;
-        pair = IUniswapV2Pair(uniswapV2Factory.createPair(address(token), IUniswapV2Factory(_uniswapV2Factory).WETH()));
+        liquidityPoolAddress = _liquidityPoolAddress;
+        feePercentage = _feePercentage;
     }
 
-    IPriceOracle public priceOracle = IPriceOracle(priceOracleAddress);
-
-    interface IPriceOracle {
-        function getLatestPrice() external view returns (int);
+    function setFeePercentage(uint256 _feePercentage) external onlyOwner {
+        require(_feePercentage <= 100, "Fee percentage cannot exceed 100");
+        feePercentage = _feePercentage;
     }
 
-    IUniswapRouterV2 public uniswapRouter;
-    
-    interface IUniswapRouterV2 {
-        function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);
-    }
-
-    address[] public path;
-
-    function setUniswapRouter(address _router) external onlyOwner {
-        uniswapRouter = IUniswapRouterV2(_router);
-        path = [address(token), IUniswapV2Factory(uniswapV2Factory).WETH()];
-    }
-
-    function addLiquidity(uint256 amount) external onlyOwner {
-        uint256 ethAmount = address(this).balance;
-        require(ethAmount > 0, "No ETH to add");
+    function addLiquidityToPool(uint256 amount) external onlyOwner {
         require(amount > 0, "Amount must be greater than 0");
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer of token failed");
         
-        int latestPrice = priceOracle.getLatestPrice();
+        uint256 fee = (amount * feePercentage) / 100;
+        uint256 depositAmount = amount - fee;
+        
+        require(IERC20(liquidityPoolAddress).transfer(address(this), depositAmount), "Transfer to LiquidityPool failed");
     }
 
-    receive() external payable {}
-
-    function uniswapV2Call(address, uint, uint, bytes calldata) external override {
-        require(msg.sender == address(pair), "Only callable by Uniswap pair");
-        uint256 balance = token.balanceOf(address(this));
-        require(balance > 0, "No token balance to sell");
-
-        uint256 ethAmount = address(this).balance;
-        require(ethAmount > 0, "No ETH balance to sell");
-
-        uint256 liquidity = pair.balanceOf(address(this));
-        pair.burn(address(this));
-
-        uint256 ethToSend = ethAmount + liquidity;
-        payable(address(pair)).transfer(ethToSend);
-
-        require(token.transfer(address(pair), balance), "Transfer failed");
-    }
-
-    function swapTokens(uint256 tokenAmount, address targetToken, uint deadline) external {
-        require(tokenAmount > 0, "Amount must be greater than 0");
-        require(targetToken != address(0), "Invalid target token address");
-        require(uniswapRouter != IUniswapRouterV2(0), "Uniswap router is not set");
-
-        require(token.transferFrom(msg.sender, address(this), tokenAmount), "Transfer failed");
+    function removeLiquidityFromPool(uint256 amount) external onlyOwner {
+        require(amount > 0, "Amount must be greater than 0");
         
-        uint[] memory amounts = uniswapRouter.swapExactTokensForTokens(tokenAmount, 0, path, address(this), deadline);
-        uint swappedAmount = amounts[amounts.length - 1];
+        uint256 fee = (amount * feePercentage) / 100;
+        uint256 withdrawAmount = amount - fee;
         
-        require(IERC20(targetToken).transfer(msg.sender, swappedAmount), "Transfer failed");
-    }
-
-    function getPairReserves() external view returns (uint256 ethReserve, uint256 tokenReserve) {
-        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
-        (ethReserve, tokenReserve) = pair.token0() == address(token) ? (reserve1, reserve0) : (reserve0, reserve1);
+        require(IERC20(liquidityPoolAddress).transferFrom(msg.sender, address(this), withdrawAmount), "Transfer from LiquidityPool failed");
+        require(token.transfer(msg.sender, withdrawAmount), "Transfer of token failed");
     }
 }
-function addLiquidityToPool(uint256 amount) external onlyOwner {
-    require(totalSupply > 0, "No total liquidity supply");
 
-    uint256 amount1 = (amount * token1.balanceOf(address(this))) / totalSupply;
-    uint256 amount2 = (amount * token2.balanceOf(address(this))) / totalSupply;
+contract LiquidityPool is Ownable {
+    IERC20 public token1;
+    IERC20 public token2;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
+    uint256 public slippageTolerance;
+    
+    constructor(address _token1, address _token2, uint256 _slippageTolerance) {
+        token1 = IERC20(_token1);
+        token2 = IERC20(_token2);
+        slippageTolerance = _slippageTolerance;
+    }
 
-    require(balances[msg.sender] >= amount, "Insufficient liquidity balance");
+    function deposit(uint256 amount1, uint256 amount2) external {
+        require(amount1 > 0, "Amount1 must be greater than 0");
+        require(amount2 > 0, "Amount2 must be greater than 0");
+        
+        require(token1.transferFrom(msg.sender, address(this), amount1), "Transfer of token1 failed");
+        require(token2.transferFrom(msg.sender, address(this), amount2), "Transfer of token2 failed");
+        
+        uint256 liquidity = calculateLiquidity(amount1, amount2);
+        totalSupply += liquidity;
+        balances[msg.sender] += liquidity;
+    }
 
-    require(token1.transfer(msg.sender, amount1), "Transfer of token1 failed");
-    require(token2.transfer(msg.sender, amount2), "Transfer of token2 failed");
+    function withdraw(uint256 liquidity) external {
+        require(liquidity > 0, "Liquidity must be greater than 0");
+        
+        uint256 amount1 = (liquidity * token1.balanceOf(address(this))) / totalSupply;
+        uint256 amount2 = (liquidity * token2.balanceOf(address(this))) / totalSupply;
+        
+        require(token1.transfer(msg.sender, amount1), "Transfer of token1 failed");
+        require(token2.transfer(msg.sender, amount2), "Transfer of token2 failed");
+        
+        totalSupply -= liquidity;
+        balances[msg.sender] -= liquidity;
+    }
 
-    totalSupply -= amount;
-    balances[msg.sender] -= amount;
+    function calculateLiquidity(uint256 amount1, uint256 amount2) internal view returns (uint256) {
+        uint256 balance1 = token1.balanceOf(address(this));
+        uint256 balance2 = token2.balanceOf(address(this));
+        return (totalSupply == 0)
+            ? (amount1 * amount2)
+            : (amount1 * totalSupply) / balance1;
+    }
+    
+    function getReserves() external view returns (uint256 reserve1, uint256 reserve2) {
+        return (token1.balanceOf(address(this)), token2.balanceOf(address(this)));
+    }
+
+    function swapTokens(address targetToken, uint256 amount) external onlyOwner {
+        require(targetToken != address(0), "Invalid target token address");
+        
+        uint256 amount1 = (amount * token1.balanceOf(address(this))) / totalSupply;
+        uint256 amount2 = (amount * token2.balanceOf(address(this))) / totalSupply;
+        
+        require(token1.transfer(targetToken, amount1), "Transfer of token1 failed");
+        require(token2.transfer(targetToken, amount2), "Transfer of token2 failed");
+        
+        totalSupply -= amount;
+    }
+
+    function setSlippageTolerance(uint256 _slippageTolerance) external onlyOwner {
+        require(_slippageTolerance <= 10, "Slippage tolerance cannot exceed 10%");
+        slippageTolerance = _slippageTolerance;
+    }
 }
 
